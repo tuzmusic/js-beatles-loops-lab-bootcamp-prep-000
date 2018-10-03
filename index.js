079295c2a0cae8232d72e690c365cff1dd84daec
Jonathan Tuzman <tuzmusic@gmail.com>
2018-10-03 16:09:40 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 function theBeatlesPlay(musicians, instruments) {
   var array = []
-  for (i=0;i<musicians.length;i++) {
+  for (var i=0;i<musicians.length;i++) {
     array.push(`${musicians[i]} plays ${instruments[i]}`)
   }
 }
